ddbce92ede9ae15cb2d57afecb603e709e5f3fb9
Fadi Ksho <fadiksho@gmail.com>
2019-08-10 19:54:11 +0100

    fixed untracked files

--- src/MyBlog/wwwroot/dist/0.js
+++
@@ -1,5 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{"./Pages/BlogDetail/blog-detail.js":
-/*!*****************************************!*\
-  !*** ./Pages/BlogDetail/blog-detail.js ***!
-  \*****************************************/
-/*! no static exports found */function(module,exports){eval('window.addEventListener("load", function () {\n  document.getElementById("scrolToButton").addEventListener("click", function () {\n    window.scroll({\n      behavior: "smooth",\n      left: 0,\n      top: pwaInstallButton.offsetTop\n    });\n  });\n});\n\n//# sourceURL=webpack:///./Pages/BlogDetail/blog-detail.js?')}}]);
